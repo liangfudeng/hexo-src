2c91b6a3146eadfdc3c59d760c51db8e951da3af
dengliangfu <liangfu.deng@gmail.com>
2018-05-11 11:43:17 +0800

    update

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-#npm install hexo-cli -g
-#npm install hexo-generator-sitemap --save
-#npm install hexo-generator-baidu-sitemap --save
-#npm install hexo-generator-searchdb --save
+npm install hexo-cli -g
+npm install hexo-generator-sitemap --save
+npm install hexo-generator-baidu-sitemap --save
+npm install hexo-generator-searchdb --save
 git clone git@github.com:liangfudeng/liangfudeng.github.io public
-#git clone git@github.com:liangfudeng/hexo-theme-next.git themes/next
+git clone git@github.com:liangfudeng/hexo-theme-next.git themes/next
 
 
